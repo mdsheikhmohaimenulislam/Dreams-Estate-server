d871a59cddcfeb41cf491f92a2b183bcb910b649
Mohai menul islam <mdsheikhmohaimenulislam@gmail.com>
2025-07-09 16:30:04 +0600

    my properties section deleted button done

--- index.js
+++ index.js
@@ -174,12 +174,12 @@ async function run() {
 
 
   // Deleted section
-//   app.delete("/properties/:id", async (req, res) => {
-//     const id = req.params.id;
-//     const query = { _id: new ObjectId(id) };
-//     const result = propertiesCollection.deleteOne(query);
-//     res.send(result);
-//   });
+  app.delete("/properties/:id", async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const result = propertiesCollection.deleteOne(query);
+    res.send(result);
+  });
 
 
 
